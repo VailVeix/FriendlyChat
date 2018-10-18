a53ea3f90a0d623c72872961706af1d8d045f345
Alex Kasa <vailveix@gmail.com>
2018-10-18 16:04:39 -0400

    sign out, sign in back thing

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.google.firebase.udacity.friendlychat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -221,7 +222,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.sign_out_menu
+                // sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     @Override
